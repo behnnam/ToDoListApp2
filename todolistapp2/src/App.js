4296eab6c0c090bb68fbee28a990ad6213512e9d
BEHNAM rashedi <behnam4rashedi@gmail.com>
2022-03-28 20:46:26 +0430

    creatte app

---
+++ todolistapp2/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import Header from './Header';
+import Body from './Body';
+import Footer from './Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Body/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
